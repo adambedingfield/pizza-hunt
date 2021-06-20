51e383ebc7b97f721c2dbcecbc5e3c98aa3dedfe
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-06-19 22:50:45 -0700

    added comment and reply functionality, recipes saved when offline through idb

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
+
+// /api/comment/<pizzaid>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaid>/<commentid>
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment)
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
+
+module.exports = router;
